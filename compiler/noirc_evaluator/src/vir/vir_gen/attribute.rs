9df88347130088eedcc9fd1cdc89ce51ca5bd57c
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-05-23 11:44:57 +0300

    chore(ast-to-vir): Skip implementation for FV annotations

--- compiler/noirc_evaluator/src/vir/vir_gen/attribute.rs
+++ compiler/noirc_evaluator/src/vir/vir_gen/attribute.rs
@@ -1,10 +1,12 @@
+use std::sync::Arc;
+
 use noirc_frontend::monomorphization::ast::Function;
 use vir::ast::Exprs;
 
 pub fn func_requires_to_vir_expr(func: &Function) -> Exprs {
-    todo!()
+    Arc::new(Vec::new()) //TODO(totel)
 }
 
 pub fn func_ensures_to_vir_expr(func: &Function) -> Exprs {
-    todo!()
+    Arc::new(Vec::new()) //TODO(totel)
 }
