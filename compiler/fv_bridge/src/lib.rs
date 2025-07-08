d5acc73abf0caac8970a5249854ca4ecf6619b08
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-07-08 18:49:50 +0300

    feat(fv-bridge): First steps towards new architecture
    Start working towards using Noir as a library without modifying internal
    structures (like extending the AST).
    Defined a new module `fv-bridge` which contains the functions for
    compiling a program and converting it to a VIR Krate.

---
+++ compiler/fv_bridge/src/lib.rs
@@ -0,0 +1,205 @@
+use std::collections::{BTreeMap, HashMap};
+
+use fm::FileId;
+use formal_verification::parse::{Attribute, parse_attribute};
+use iter_extended::vecmap;
+use noirc_driver::{CompilationResult, CompileError, CompileOptions, check_crate};
+use noirc_errors::{CustomDiagnostic, Location};
+use noirc_evaluator::{
+    errors::{RuntimeError, SsaReport},
+    vir::{create_verus_vir, vir_gen::BuildingKrateError},
+};
+use noirc_frontend::{
+    debug::DebugInstrumenter,
+    graph::CrateId,
+    hir::Context,
+    monomorphization::{
+        Monomorphizer,
+        ast::{FuncId, Program},
+        debug_types::DebugTypeTracker,
+        errors::MonomorphizationError,
+        perform_impl_bindings, perform_instantiation_bindings, undo_instantiation_bindings,
+    },
+    node_interner::{self, NodeInterner},
+    parser::ParserError,
+    token::SecondaryAttributeKind,
+};
+use vir::ast::Krate;
+
+pub fn compile_and_build_vir_krate(
+    context: &mut Context,
+    crate_id: CrateId,
+    options: &CompileOptions,
+) -> CompilationResult<Krate> {
+    modified_compile_main(context, crate_id, options)
+}
+
+fn modified_compile_main(
+    context: &mut Context,
+    crate_id: CrateId,
+    options: &CompileOptions,
+) -> CompilationResult<Krate> {
+    let (_, mut warnings) = check_crate(context, crate_id, options)?;
+
+    let main = context.get_main_function(&crate_id).ok_or_else(|| {
+        let err = CustomDiagnostic::from_message(
+            "cannot compile crate into a program as it does not contain a `main` function",
+            FileId::default(),
+        );
+        vec![err]
+    })?;
+
+    let compiled_program = modified_compile_no_check(context, options, main)
+        .map_err(|error| vec![CustomDiagnostic::from(error)])?;
+
+    let compilation_warnings = vecmap(compiled_program.warnings.clone(), CustomDiagnostic::from);
+    if options.deny_warnings && !compilation_warnings.is_empty() {
+        return Err(compilation_warnings);
+    }
+    if !options.silence_warnings {
+        warnings.extend(compilation_warnings);
+    }
+
+    Ok((compiled_program.krate, warnings))
+}
+
+// Something like the method `compile_no_check()`
+fn modified_compile_no_check(
+    context: &mut Context,
+    options: &CompileOptions,
+    main_function: node_interner::FuncId,
+) -> Result<KrateAndWarnings, CompileError> {
+    let force_unconstrained = options.force_brillig || options.minimal_ssa;
+
+    let (program, fv_annotations) = modified_monomorphize(
+        main_function,
+        &mut context.def_interner,
+        &DebugInstrumenter::default(),
+        force_unconstrained,
+    )?;
+
+    if options.show_monomorphized {
+        println!("{program}");
+    }
+
+    Ok(KrateAndWarnings {
+        krate: create_verus_vir(program).map_err(|BuildingKrateError::Error(msg)| {
+            RuntimeError::InternalError(noirc_evaluator::errors::InternalError::General {
+                message: msg,
+                call_stack: vec![],
+            })
+        })?,
+        warnings: vec![],
+        parse_annotations_errors: vec![], // TODO(totel): Get the errors from `modified_monomorphize()`
+    })
+}
+
+enum MonomorphOrParserError {
+    MonomorphizationError(MonomorphizationError),
+    ParserErrors(Vec<ParserError>),
+}
+
+fn modified_monomorphize(
+    main: node_interner::FuncId,
+    interner: &mut NodeInterner,
+    debug_instrumenter: &DebugInstrumenter,
+    force_unconstrained: bool,
+) -> Result<(Program, Vec<(FuncId, Vec<Attribute>)>), MonomorphizationError> {
+    let debug_type_tracker = DebugTypeTracker::build_from_debug_instrumenter(debug_instrumenter);
+    // TODO(totel): Monomorphizer is a `pub(crate)` struct
+    let mut monomorphizer = Monomorphizer::new(interner, debug_type_tracker);
+    monomorphizer.in_unconstrained_function = force_unconstrained;
+    let function_sig = monomorphizer.compile_main(main)?;
+    let mut new_ids_to_old_ids: HashMap<FuncId, node_interner::FuncId> = HashMap::new();
+
+    while !monomorphizer.queue.is_empty() {
+        let (next_fn_id, new_id, bindings, trait_method, is_unconstrained, location) =
+            monomorphizer.queue.pop_front().unwrap();
+        monomorphizer.locals.clear();
+
+        monomorphizer.in_unconstrained_function = is_unconstrained;
+
+        perform_instantiation_bindings(&bindings);
+        let interner = &monomorphizer.interner;
+        let impl_bindings = perform_impl_bindings(interner, trait_method, next_fn_id, location)
+            .map_err(MonomorphizationError::InterpreterError)?;
+
+        monomorphizer.function(next_fn_id, new_id, location)?;
+        new_ids_to_old_ids.insert(new_id, next_fn_id);
+        undo_instantiation_bindings(impl_bindings);
+        undo_instantiation_bindings(bindings);
+    }
+
+    let func_sigs = monomorphizer
+        .finished_functions
+        .iter()
+        .flat_map(|(_, f)| {
+            if (!force_unconstrained && f.inline_type.is_entry_point())
+                || f.id == Program::main_id()
+            {
+                Some(f.func_sig.clone())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let globals = monomorphizer.finished_globals.into_iter().collect::<BTreeMap<_, _>>();
+
+    let fv_annotations: Vec<(FuncId, Vec<Attribute>)> = monomorphizer
+        .finished_functions
+        .iter()
+        .filter_map(|(new_func_id, function)| {
+            new_ids_to_old_ids.get(new_func_id).map(|old_id| (new_func_id, old_id, function))
+        })
+        .map(|(new_func_id, old_id, function)| {
+            let tag_attributes: Vec<(&str, Location)> = monomorphizer
+                .interner
+                .function_attributes(old_id)
+                .secondary
+                .iter()
+                .filter_map(|attribute| match &attribute.kind {
+                    SecondaryAttributeKind::Tag(annotation) => {
+                        Some((annotation.as_str(), attribute.location))
+                    }
+                    _ => None,
+                })
+                .collect();
+
+            (
+                new_func_id.clone(),
+                tag_attributes
+                    .into_iter()
+                    .map(|(annotation_body, location)| {
+                        //TODO(totel): After error types are introduced remove the `unwrap` and switch to `?`
+                        parse_attribute(annotation_body, location, function, &globals).unwrap()
+                    })
+                    .collect(),
+            )
+        })
+        .collect();
+
+    let functions = vecmap(monomorphizer.finished_functions, |(_, f)| f);
+
+    let (debug_variables, debug_functions, debug_types) =
+        monomorphizer.debug_type_tracker.extract_vars_and_types();
+
+    let program = Program::new(
+        functions,
+        func_sigs,
+        function_sig,
+        monomorphizer.return_location,
+        globals,
+        debug_variables,
+        debug_functions,
+        debug_types,
+    );
+
+    Ok((program.handle_ownership(), fv_annotations))
+}
+
+pub struct KrateAndWarnings {
+    pub krate: Krate,
+    pub warnings: Vec<SsaReport>,
+    pub parse_annotations_errors: Vec<ParserError>,
+}
