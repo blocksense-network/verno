22ddf183a738293a7173757fb2b18bfb9a11432a
reo101 <pavel.atanasov2001@gmail.com>
2025-07-14 07:39:35 +0300

    feat(annotations): check if integer literals fit in their holes
    - Create a `bi_can_fit_in` function that finds the smallest type, in
      which a `BigInteger` can fit and checks if that fits inside a
      predetermined type.
    - Use that to verify if all integer literals fit in their inferred types
    - Add a big test that tests all edge cases

--- compiler/formal_verification/src/typing.rs
+++ compiler/formal_verification/src/typing.rs
@@ -5,12 +5,76 @@ use crate::{
         UnaryOp, cata, try_cata, try_contextual_cata,
     },
 };
-use noirc_frontend::monomorphization::{FUNC_RETURN_VAR_NAME, ast::Type as NoirType};
+use noirc_frontend::{
+    ast::IntegerBitSize,
+    monomorphization::{FUNC_RETURN_VAR_NAME, ast::Type as NoirType},
+    shared::Signedness,
+};
+use num_bigint::{BigInt, BigUint};
+use num_traits::{One, Zero};
 
 #[derive(Debug, Clone)]
 pub enum TypeInferenceError {
     MonomorphizationRequest(MonomorphizationRequest),
     TypeError { got: Option<NoirType>, wanted: Option<NoirType>, message: Option<String> },
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum FitsIn {
+    Yes,
+    No {
+        /// `None` means there is no possible `IntegerBitSize` that could contain us
+        need: Option<NoirType>,
+    },
+}
+fn bi_can_fit_in(bi: &BigInt, hole_size: &IntegerBitSize, hole_sign: &Signedness) -> FitsIn {
+    let is_negative = match bi.sign() {
+        num_bigint::Sign::Minus => true,
+        num_bigint::Sign::Plus | num_bigint::Sign::NoSign => false,
+    };
+
+    // NOTE: if we have a negative literal, but try to fit into an unsigned integer
+    let is_wrong_sign = is_negative && !hole_sign.is_signed();
+
+    let mut bits = bi.bits();
+
+    // NOTE: Add one bit for the sign.
+    if hole_sign.is_signed() {
+        bits += 1;
+    }
+
+    if is_negative {
+        fn is_power_of_two(n: &BigUint) -> bool {
+            // 10000000
+            // 01111111
+            !n.is_zero() && ((n & (n - BigUint::one())) == BigUint::zero())
+        }
+
+        // NOTE: ...unless we have a negative number whose magnitude is a power of two.
+        //       This is the because it fits perfectly into the two's complement minimum value.
+        //       (e.g., -128 fits in `i8`, the same as -127, but not 128).
+        if is_power_of_two(bi.magnitude()) {
+            bits -= 1;
+        }
+    }
+
+    // NOTE: find the smallest `IntegerBitSize` that could contain us, `None` if no such one exists
+    let smallest_fit_size =
+        IntegerBitSize::allowed_sizes().into_iter().find(|size| bits <= size.bit_size() as u64);
+
+    // NOTE: using the `PartialOrd` instance for `IntegerBitSize`,
+    //       which definitionally orders the bit sizes in an increasing order
+    let size_fits = smallest_fit_size.map(|sfs| sfs <= *hole_size).unwrap_or(false);
+
+    if !size_fits || is_wrong_sign {
+        return FitsIn::No {
+            need: smallest_fit_size.map(|sfs| {
+                NoirType::Integer(if is_wrong_sign { Signedness::Signed } else { *hole_sign }, sfs)
+            }),
+        };
+    }
+
+    return FitsIn::Yes;
 }
 
 pub fn type_infer(state: State, expr: SpannedExpr) -> Result<SpannedTypedExpr, TypeInferenceError> {
@@ -98,17 +162,48 @@ pub fn type_infer(state: State, expr: SpannedExpr) -> Result<SpannedTypedExpr, T
                         | BinaryOp::Or
                         | BinaryOp::Xor => {
                             let is_arith = op.is_arithmetic();
+
+                            let propagate_concrete_type =
+                                |e: SpannedOptionallyTypedExpr, t: NoirType| {
+                                    let NoirType::Integer(hole_sign, hole_size) = &t else {
+                                        todo!("Can only propagate integer types");
+                                    };
+                                    try_cata(e, &|(location, _type), expr| {
+                                        debug_assert!(_type == None);
+                                        match expr {
+                                            ExprF::Literal { value: Literal::Int(ref bi) } => {
+                                                let fits = bi_can_fit_in(bi, hole_size, hole_sign);
+                                                match fits {
+                                                    FitsIn::Yes => {}
+                                                    FitsIn::No { need } => {
+                                                        return Err(
+                                                            TypeInferenceError::TypeError {
+                                                                got: need.clone(),
+                                                                wanted: Some(t.clone()),
+                                                                message: Some(format!(
+                                                                    "Integer literal {} cannot fit in {}, needs at least {:?} or larger",
+                                                                    bi, t, need,
+                                                                )),
+                                                            },
+                                                        );
+                                                    }
+                                                }
+                                            }
+                                            _ => {}
+                                        }
+
+                                        Ok(SpannedOptionallyTypedExpr {
+                                            expr: Box::new(expr),
+                                            ann: (location, Some(t.clone())),
+                                        })
+                                    })
+                                };
+
                             match (&expr_left.ann.1, &expr_right.ann.1) {
                                 (None, None) => (exprf, None),
                                 (None, Some(t2)) => {
                                     let expr_left_inner =
-                                        cata(expr_left.clone(), &|(location, _type), expr| {
-                                            debug_assert!(_type == None);
-                                            SpannedOptionallyTypedExpr {
-                                                expr: Box::new(expr),
-                                                ann: (location, Some(t2.clone())),
-                                            }
-                                        });
+                                        propagate_concrete_type(expr_left.clone(), t2.clone())?;
 
                                     (
                                         ExprF::BinaryOp {
@@ -121,13 +216,7 @@ pub fn type_infer(state: State, expr: SpannedExpr) -> Result<SpannedTypedExpr, T
                                 }
                                 (Some(t1), None) => {
                                     let expr_right_inner =
-                                        cata(expr_right.clone(), &|(location, _type), expr| {
-                                            debug_assert!(_type == None);
-                                            SpannedOptionallyTypedExpr {
-                                                expr: Box::new(expr),
-                                                ann: (location, Some(t1.clone())),
-                                            }
-                                        });
+                                        propagate_concrete_type(expr_right.clone(), t1.clone())?;
 
                                     (
                                         ExprF::BinaryOp {
@@ -208,11 +297,83 @@ mod tests {
     use noirc_errors::{Location, Span};
     use noirc_frontend::ast::IntegerBitSize;
     use noirc_frontend::shared::Signedness;
+    use num_traits::Num;
     use std::convert::identity;
 
     use super::*;
 
     use crate::{Attribute, ast::Literal, parse::tests::*, parse_attribute};
+
+    #[test]
+    fn test_integer_types() {
+        {
+            let bi127 = BigInt::from_str_radix("127", 10).unwrap();
+            let bi128 = BigInt::from_str_radix("128", 10).unwrap();
+            let bi129 = BigInt::from_str_radix("129", 10).unwrap();
+            let bin127 = BigInt::from_str_radix("-127", 10).unwrap();
+            let bin128 = BigInt::from_str_radix("-128", 10).unwrap();
+            let bin129 = BigInt::from_str_radix("-129", 10).unwrap();
+
+            let hole_size = IntegerBitSize::Eight;
+            let hole_sign = Signedness::Signed;
+
+            let bi127_fit = bi_can_fit_in(&bi127, &hole_size, &hole_sign);
+            let bi128_fit = bi_can_fit_in(&bi128, &hole_size, &hole_sign);
+            let bi129_fit = bi_can_fit_in(&bi129, &hole_size, &hole_sign);
+            let bin127_fit = bi_can_fit_in(&bin127, &hole_size, &hole_sign);
+            let bin128_fit = bi_can_fit_in(&bin128, &hole_size, &hole_sign);
+            let bin129_fit = bi_can_fit_in(&bin129, &hole_size, &hole_sign);
+
+            assert_eq!(bi127_fit, FitsIn::Yes);
+            assert_eq!(
+                bi128_fit,
+                FitsIn::No { need: Some(NoirType::Integer(hole_sign, IntegerBitSize::Sixteen)) }
+            );
+            assert_eq!(
+                bi129_fit,
+                FitsIn::No { need: Some(NoirType::Integer(hole_sign, IntegerBitSize::Sixteen)) }
+            );
+
+            assert_eq!(bin127_fit, FitsIn::Yes);
+            assert_eq!(bin128_fit, FitsIn::Yes);
+            assert_eq!(
+                bin129_fit,
+                FitsIn::No { need: Some(NoirType::Integer(hole_sign, IntegerBitSize::Sixteen)) }
+            );
+        }
+
+        {
+            let bi255 = BigInt::from_str_radix("255", 10).unwrap();
+            let bi256 = BigInt::from_str_radix("256", 10).unwrap();
+            let bi257 = BigInt::from_str_radix("257", 10).unwrap();
+            let bin1 = BigInt::from_str_radix("-1", 10).unwrap();
+
+            let hole_size = IntegerBitSize::Eight;
+            let hole_sign = Signedness::Unsigned;
+
+            let bi255_fit = bi_can_fit_in(&bi255, &hole_size, &hole_sign);
+            let bi256_fit = bi_can_fit_in(&bi256, &hole_size, &hole_sign);
+            let bi257_fit = bi_can_fit_in(&bi257, &hole_size, &hole_sign);
+            let bin1_fit = bi_can_fit_in(&bin1, &hole_size, &hole_sign);
+
+            assert_eq!(bi255_fit, FitsIn::Yes);
+            assert_eq!(
+                bi256_fit,
+                FitsIn::No { need: Some(NoirType::Integer(hole_sign, IntegerBitSize::Sixteen)) }
+            );
+            assert_eq!(
+                bi257_fit,
+                FitsIn::No { need: Some(NoirType::Integer(hole_sign, IntegerBitSize::Sixteen)) }
+            );
+
+            assert_eq!(
+                bin1_fit,
+                FitsIn::No {
+                    need: Some(NoirType::Integer(Signedness::Signed, IntegerBitSize::One))
+                }
+            );
+        }
+    }
 
     #[test]
     fn test_whole_attribute() {
